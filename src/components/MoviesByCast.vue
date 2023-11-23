92c1d3bbe663c183f5a359837ff29a91b6194d4a
Marcin Strzelec <m.strzelec21052003@gmail.com>
2023-11-23 15:59:04 +0100

    finish2

--- src/components/MoviesByCast.vue
+++ src/components/MoviesByCast.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="my-3">
 		<div class="form-group input-center row my-3">
-			<label for="inputCast">Cast:</label>
+			<label for="inputFilterCast">Cast:</label>
 			<div class="col-sm-12">
-				<input type="text" v-model="castQuery" id="inputCast" class="form-control"
+				<input type="text" v-model="filterCast" id="inputFilterCast" class="form-control"
 					placeholder="First and last name" />
 			</div>
 		</div>
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import _ from "lodash"
+import _ from 'lodash';
 
 export default {
 	name: 'MoviesByCast',
@@ -28,7 +28,7 @@ export default {
 	},
 	data() {
 		return {
-			castQuery: ''
+			filterCast: '',
 		};
 	},
 	computed: {
@@ -43,11 +43,15 @@ export default {
 				});
 			});
 
-			const filteredGrouped = this.castQuery
-				? _.pickBy(grouped, (movies, castMember) =>
-					_.toLower(castMember).includes(_.toLower(this.castQuery))
+			const filteredGrouped = this.filterCast
+				? _.pickBy(grouped, (_movies, castMember) =>
+					_.toLower(castMember).includes(_.toLower(this.filterCast))
 				)
 				: grouped;
+
+			_.forEach(filteredGrouped, (movies, castMember) => {
+				filteredGrouped[castMember] = _.orderBy(movies, 'title', 'asc');
+			});
 
 			return _(filteredGrouped)
 				.toPairs()
--- src/components/MoviesByGenre.vue
+++ src/components/MoviesByGenre.vue
@@ -1,12 +1,13 @@
 <template>
-	<div class="form-group input-center row my-3">
-		<label for="inputFilterGenre">Genre:</label>
-		<div class="col-sm-12">
-			<input type="text" v-model="filterGenre" id="inputFilterGenre" class="form-control" placeholder="Move genre name" />
-		</div>
-	</div>
 	<div class="my-3">
-		<div v-for="(movies, genre) in filteredGenres" :key="genre" class="card">
+		<div class="form-group input-center row my-3">
+			<label for="inputFilterGenre">Genre:</label>
+			<div class="col-sm-12">
+				<input type="text" v-model="filterGenre" id="inputFilterGenre" class="form-control"
+					placeholder="Move genre name" />
+			</div>
+		</div>
+		<div v-for="(movies, genre) in moviesByGenre" :key="genre" class="card">
 			<div class="card-header border-dark">{{ genre }}</div>
 			<ol class="list-group list-group-numbered">
 				<li v-for="(movie, index) in movies" :key="index" class="list-group-item list-group-item-action">
@@ -31,7 +32,7 @@ export default {
 		};
 	},
 	computed: {
-		filteredGenres() {
+		moviesByGenre() {
 			const grouped = {};
 			this.movies.forEach(movie => {
 				movie.genres.forEach(genre => {
@@ -43,10 +44,14 @@ export default {
 			});
 
 			const filteredGrouped = this.filterGenre
-				? _.pickBy(grouped, (movies, genre) =>
+				? _.pickBy(grouped, (_movies, genre) =>
 					_.toLower(genre).includes(_.toLower(this.filterGenre))
 				)
 				: grouped;
+
+			_.forEach(filteredGrouped, (movies, genre) => {
+				filteredGrouped[genre] = _.orderBy(movies, 'title', 'asc');
+			});
 
 			return _(filteredGrouped)
 				.toPairs()
